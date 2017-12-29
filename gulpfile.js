6e8c9070c5b159e16b701c0015c9a761f54b274d
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-12-29 13:21:56 -0500

    Configuracion inicial de Gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+// Test gulpfile
+//console.log("Gulpfile just run !");
+
+// const --> ES2015
+// require() --> CommonJS
+const gulp = require("gulp");
+// Default task for Gulp
+gulp.task("default", () => {
+    console.log("Gulpfile just run !");
+});
